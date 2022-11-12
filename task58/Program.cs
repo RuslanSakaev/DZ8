2d16cffdea44fd3b306960cf800b41b69e76dd58
Ruslan Sakaev <hardist@mail.ru>
2022-11-13 00:31:51 +0300

    Добавил решение задачи 58

--- task58/Program.cs
+++ task58/Program.cs
@@ -1,2 +1,100 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 58: 
+Задайте две матрицы. 
+Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18
+*/
+
+Console.WriteLine($"Задайте размер двумерного массива: ");
+int m = InputNumbers("Задайте количество строк: ");
+int n = InputNumbers("Задайте количество колонок: ");
+int a = InputNumbers("Задайте числа для заполнения массива: от ");
+int b = InputNumbers("Задайте числа для заполнения массива: до ");
+
+int InputNumbers(string input) // перевод введённых значений в числа
+
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+int[,] array2D1 = firstMatrix(m, n, a, b); //задали размер массива заполненный случавйными числами в диапозоне от a до b
+Console.WriteLine("Первая матрица");
+PrintMatrix(array2D1);
+
+int[,] firstMatrix(int rows, int columns, int min, int max) //создали массив заполненный случавйными числами
+{
+    int[,] matrix = new int[rows, columns];
+    var rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) // - rows,  строки 
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // - columns, колонки
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+int[,] array2D2 = secondMatrix(m, n, a, b); //задали размер массива заполненный случавйными числами в диапозоне от a до b
+Console.WriteLine("Вторая матрица");
+PrintMatrix(array2D2);
+
+int[,] secondMatrix(int rows, int columns, int min, int max) //создали массив заполненный случавйными числами
+{
+    int[,] matrix = new int[rows, columns];
+    var rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) // - rows,  строки 
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // - columns, колонки
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+
+void PrintMatrix(int[,] matrix) //печать массива
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4} ");
+            else Console.Write($"{matrix[i, j], 4} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Произведение двух матриц");
+
+int[,] resalt = MatrixMultiplication(array2D1, array2D2);
+PrintMatrix(resalt);
+
+int[,] MatrixMultiplication(int[,] firstMatrix, int[,] secondMatrix)
+{
+    if (firstMatrix.GetLength(1) != secondMatrix.GetLength(0)) throw new Exception("Матрицы нельзя пеермножить");  
+    int[,] resalt = new int[firstMatrix.GetLength(0), secondMatrix.GetLength(1)];
+    for (int i = 0; i < firstMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < secondMatrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < secondMatrix.GetLength(0); k++)
+            {
+                resalt[i, j] += firstMatrix[i, k] * secondMatrix[k, j];
+            }
+        }
+    }
+    return resalt;
+}
--- task60/Program.cs
+++ task60/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 60. 
+// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 27(0,0,1) 25(0,1,0) 90(0,1,1)
+// 34(1,0,0) 26(1,0,1) 41(1,1,0) 55(1,1,1)
+
+Console.WriteLine($"Задайте размер трёхмерного массива: ");
+int m = InputNumbers("Задайте количество строк: ");
+int n = InputNumbers("Задайте количество колонок: ");
+int a = InputNumbers("Задайте числа для заполнения массива: от ");
+int b = InputNumbers("Задайте числа для заполнения массива: до ");
+
+int InputNumbers(string input) // перевод введённых значений в числа
+
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+// Console.WriteLine("Введите размерность второй матрицы");
+// int[,] secondMatrix = new int[Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())];
+// for (int i = 0; i < secondMatrix.GetLength(0); i++)
+// {
+//     for (int j = 0; j < secondMatrix.GetLength(1); j++)
+//     {
+//         Console.Write("Заполните массив цифрами[{0},{1}] = ", i, j);
+//         secondMatrix[i, j] = Convert.ToInt32(Console.ReadLine());
+//     }
+// }
